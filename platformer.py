6ab13de6a573552beabed078cb620c2b5df70f0a
lee-vivian <lee-vivian@users.noreply.github.com>
2020-02-12 18:16:46 -0500

    Add background image

---
+++ platformer.py
@@ -0,0 +1,49 @@
+'''
+Basic Platform Game
+author: Vivian Lee
+created: 02-12-2020
+acknowledgements: followed tutorial from opensource.com
+'''
+
+import pygame
+import sys
+import os
+
+'''
+Objects
+'''
+
+
+'''
+Setup
+'''
+worldx = 960
+worldy = 720
+fps = 40  # frame rate
+ani = 4  # animation cycles
+clock = pygame.time.Clock()
+pygame.init()
+world = pygame.display.set_mode([worldx, worldy])
+backdrop = pygame.image.load(os.path.join('images', 'platform_bkgd.png')).convert()
+backdropbox = world.get_rect()
+
+'''
+Main Loop
+'''
+
+main = True
+while main is True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            main = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == ord('q'):
+                pygame.quit()
+                main = False
+                sys.exit()
+
+        world.blit(backdrop, backdropbox)
+        pygame.display.flip()
+        clock.tick(fps)
