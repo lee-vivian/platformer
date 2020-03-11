aa935b563db4f139c67151f34935b802103dbc8d
lee-vivian <lee-vivian@users.noreply.github.com>
2020-03-11 14:45:06 -0400

    Update arg options in main.py

--- main.py
+++ main.py
@@ -13,26 +13,44 @@ B. Play a level [use python3 interpreter]
 
 import argparse
 
+from utils import error_exit
 
-def main(game, level, player_img, process):
 
-    if process:
-        import enumerate
-        import extract_metatiles
-        import get_metatile_id_map
-        import extract_constraints
-        import get_states_per_metatile
+def main(game, level, player_img, enumerate, extract_metatiles, get_metatile_id_map, get_states_per_metatile,
+         extract_constraints, process_all):
 
-        enumerate.main(game, level, player_img)
-        extract_metatiles.main(game, level, player_img, print_stats=True)
-        get_states_per_metatile.main([game], [level], player_img, merge=False, outfile=None)
-        id_metatile_file, metatile_id_file = get_metatile_id_map.main([game], [level], player_img, outfile=None)
-        tileset_constraints_file = extract_constraints.main(metatile_id_file, [game], [level], player_img, outfile=None)
-        return tileset_constraints_file
+    any_processing = process_all or (enumerate or extract_metatiles or get_metatile_id_map or get_states_per_metatile
+                                     or extract_constraints)
+
+    if any_processing:
+
+        id_metatile_file, metatile_id_file = None, None
+
+        if process_all or enumerate:
+            import enumerate
+            enumerate.main(game, level, player_img)
+
+        if process_all or extract_metatiles:
+            import extract_metatiles
+            extract_metatiles.main(game, level, player_img, print_stats=True)
+
+        if process_all or get_metatile_id_map:
+            import get_metatile_id_map
+            id_metatile_file, metatile_id_file = get_metatile_id_map.main([game], [level], player_img, outfile=None)
+
+        if process_all or get_states_per_metatile:
+            import get_states_per_metatile
+            get_states_per_metatile.main([game], [level], player_img, merge=False, outfile=None)
+
+        if process_all or extract_constraints:
+            if metatile_id_file is None:
+                error_exit("The metatile_id file for %s: level %s does not exist. "
+                           "Run get_metatile_id_map script first." % (game, level))
+            import extract_constraints
+            extract_constraints.main(metatile_id_file, [game], [level], player_img, outfile=None)
 
     else:
         import platformer
-
         platformer.main(game, level, player_img, use_graph=False, draw_labels=False, draw_dup_labels_only=False)
 
 
@@ -48,13 +66,22 @@ if __name__ == "__main__":
     # ]
     #
     # for game, level in GAME_LEVEL_PAIRS:
-    #     main(game, level, 'block', True)
+    #     main(game, level, player_img='block', enumerate=False, extract_metatiles=True, get_metatile_id_map=True,
+    #          get_states_per_metatile=True, extract_constraints=True, process_all=False)
 
     parser = argparse.ArgumentParser(description='Process or play a level')
     parser.add_argument('game', type=str, help='Name of the game')
     parser.add_argument('level', type=str, help='Name of the level')
     parser.add_argument('--player_img', type=str, help='Player image', default='block')
-    parser.add_argument('--process', const=True, nargs='?', type=bool, help="Run process scripts for the given level", default=False)
+    parser.add_argument('--enumerate', const=True, nargs='?', type=bool, default=False)
+    parser.add_argument('--extract_metatiles', const=True, nargs='?', type=bool, default=False)
+    parser.add_argument('--get_metatile_id_map', const=True, nargs='?', type=bool, default=False)
+    parser.add_argument('--extract_constraints', const=True, nargs='?', type=bool, default=False)
+    parser.add_argument('--get_states_per_metatile', const=True, nargs='?', type=bool, default=False)
+    parser.add_argument('--process_all', const=True, nargs='?', type=bool,
+                        help="Run all process scripts for the given level", default=False)
+
     args = parser.parse_args()
 
-    main(args.game, args.level, args.player_img, args.process)
+    main(args.game, args.level, args.player_img, args.enumerate, args.extract_metatiles, args.get_metatile_id_map,
+         args.extract_constraints, args.get_states_per_metatile, args.process_all)
