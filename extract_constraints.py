4092c92db927d194b506accfa28ecf131765221a
lee-vivian <lee-vivian@users.noreply.github.com>
2020-04-09 16:12:05 -0400

    Save work on updating extract_constraints.py

--- extract_constraints.py
+++ extract_constraints.py
@@ -1,25 +1,43 @@
-'''
-Generates a json file representing a tileset and the constraints for each tile's 8 neighbor positions
-'''
+"""
+Generates a pickle file representing a tileset and the constraints for each tile's 8 neighbor positions
+"""
 
 import argparse
 import os
-import datetime
+from datetime import datetime
 
 from model.level import Level, TILE_DIM
 from model.metatile import Metatile, TYPE_IMG_MAP
-import utils
+from utils import read_pickle, write_pickle, get_filepath
 
 TILE_TOP_LEFT = "0,0"
 
-LEFT = "-1,0"
-RIGHT = "1,0"
-TOP = "0,-1"
-BOTTOM = "0,1"
-TOP_LEFT = "-1,-1"
-BOTTOM_LEFT = "-1,1"
-TOP_RIGHT = "1,-1"
-BOTTOM_RIGHT = "1,1"
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+TOP = (0, -1)
+BOTTOM = (0, 1)
+TOP_LEFT = (-1, -1)
+BOTTOM_LEFT = (-1, 1)
+TOP_RIGHT = (1, -1)
+BOTTOM_RIGHT = (1, 1)
+NEIGHBOR_COORDS = [LEFT, RIGHT, TOP, BOTTOM, TOP_LEFT, BOTTOM_LEFT, TOP_RIGHT, BOTTOM_RIGHT]
+
+
+# def get_tileset_constraints_dict(coord_metatiles_dict):
+#
+#     for coord, metatiles in coord_metatiles_dict.items()
+#
+#     neighbor_coords = [(x * TILE_DIM, y * TILE_DIM) for (x, y) in NEIGHBOR_COORDS]
+
+
+
+
+
+
+
+
+
+
 
 
 # [ ] [ ] [ ]
@@ -174,7 +192,81 @@ def merge_tileset_dicts(combined_tileset_dict, level_tileset_dict):
     }
 
 
-def main(metatile_id_file, games, levels, player_img, load_saved_files, outfile):
+# TODO updated functions from here below
+
+def get_coord_metatiles_dict(metatile_coords_dicts):
+    # Get all metatiles at each coord
+    coord_metatiles_dict_tmp = {}
+    for metatile_coord_dict in metatile_coords_dicts:
+        for metatile_str, coords in metatile_coord_dict.items():
+            metatile = Metatile.from_str(metatile_str)
+            for coord in coords:
+                if coord_metatiles_dict_tmp.get(coord) is None:
+                    coord_metatiles_dict_tmp[coord] = []
+                coord_metatiles_dict_tmp[coord].append(metatile)
+
+    print("removing duplicates...")
+
+    # Remove duplicate metatiles
+    coord_metatiles_dict = {}
+    for coord, metatiles in coord_metatiles_dict_tmp.items():
+        coord_metatiles_dict[coord] = Metatile.get_unique_metatiles(metatiles)
+    
+    return coord_metatiles_dict
+
+
+def get_coord_tile_ids_dict(metatile_id_map, coord_metatiles_dict):
+    ordered_metatiles = []
+    ordered_ids = []
+    for metatile_str, id in metatile_id_map.items():
+        ordered_metatiles.append(Metatile.from_str(metatile_str))
+        ordered_ids.append(id)
+
+    coord_tile_ids_dict = {}
+    for coord, metatiles in coord_metatiles_dict.items():
+        coord_tile_ids_dict[coord] = []
+        for metatile in metatiles:
+            tile_id_idx = ordered_metatiles.index(metatile)
+            tile_id = ordered_ids[tile_id_idx]
+            coord_tile_ids_dict[coord].append(tile_id)
+
+    return coord_tile_ids_dict
+    
+
+def main(save_filename, metatile_id_map_file, metatile_coords_dict_files, player_img):
+
+    print("Constructing tileset constaints dictionary...")
+    start_time = datetime.now()
+
+    metatile_constraints_dir = "level_saved_files_%s/metatile_constraints" % player_img
+    metatile_constraints_file = get_filepath(metatile_constraints_dir, "%s.pickle" % save_filename)
+
+    metatile_id_map = read_pickle(metatile_id_map_file)
+    metatile_coords_dicts = [read_pickle(file) for file in metatile_coords_dict_files]
+
+    print("get_coord_metatiles_dict...")
+    start_time = datetime.now()
+    coord_metatiles_dict = get_coord_metatiles_dict(metatile_coords_dicts)
+    print("time: %s" % str(datetime.now()-start_time))
+
+    print("get_coord_tile_ids_dict...")
+    start_time = datetime.now()
+    coord_tile_ids_dict = get_coord_tile_ids_dict(metatile_id_map, coord_metatiles_dict)
+    print("time: %s" % str(datetime.now() - start_time))
+
+    print(coord_tile_ids_dict)
+
+    return
+
+    tileset_constraints_dict = get_tileset_constraints_dict(metatile_id_map, metatile_coords_dicts)
+
+    end_time = datetime.now()
+    print("Runtime: %s" % str(end_time-start_time))
+
+    write_pickle(metatile_constraints_file, tileset_constraints_dict)
+    return metatile_constraints_file
+
+    exit(0)
 
     metatile_constraints_dir = utils.get_directory("level_saved_files_%s/metatile_constraints" % player_img)
     game_level_pairs = zip(games, levels)
@@ -211,13 +303,48 @@ def main(metatile_id_file, games, levels, player_img, load_saved_files, outfile)
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Extract combined tileset constraints json for the specified levels')
-    parser.add_argument('metatile_id_file', type=str, help='Filepath for metatile_id map')
-    parser.add_argument('--games', type=str, nargs="+", help='List of games', default="")
-    parser.add_argument('--levels', type=str, nargs="+", help='List of game levels', default="")
-    parser.add_argument('--player_img', type=str, help="Player image", default='block')
-    parser.add_argument('--load_saved_files', const=True, nargs='?', type=bool, default=False)
-    parser.add_argument('--outfile', type=str, help="Output filename", default=None)
-    args = parser.parse_args()
+    GAME_LEVEL_PAIRS = [
+        ('super_mario_bros', 'mario-1-1'),
+        ('super_mario_bros', 'mario-2-1'),
+        ('super_mario_bros', 'mario-3-1'),
+        ('super_mario_bros', 'mario-3-2'),
+        ('super_mario_bros', 'mario-4-1'),
+        ('super_mario_bros', 'mario-5-1'),
+        ('super_mario_bros', 'mario-5-2'),
+        ('super_mario_bros', 'mario-6-1'),
+        ('super_mario_bros', 'mario-6-2'),
+        ('super_mario_bros', 'mario-7-1'),
+        ('super_mario_bros', 'mario-8-1'),
+        ('super_mario_bros', 'mario-8-2'),
+        ('super_mario_bros', 'mario-8-3'),
+        ('kid_icarus', 'kidicarus_1')
+    ]
 
-    main(args.metatile_id_file, args.games, args.levels, args.player_img, args.load_saved_files, args.outfile)
+    metatile_id_map_file = "level_saved_files_block/metatile_id_maps/combined_levels.pickle"
+    metatile_coords_dict_files = []
+    for game, level in GAME_LEVEL_PAIRS:
+        file = "level_saved_files_block/metatile_coords_dicts/%s/%s.pickle" % (game, level)
+        metatile_coords_dict_files.append(file)
+
+    main('blah', metatile_id_map_file, metatile_coords_dict_files, 'block')
+    exit(0)
+
+
+    parser = argparse.ArgumentParser(description='Extract combined tileset constraints for the specified levels')
+    parser.add_argument('save_filename', type=str, help='File name to save tileset constraints to')
+    parser.add_argument('metatile_id_map_file', type=str, help='Filepath for metatile_id map')
+    parser.add_argument('metatile_coords_dict_files', type=str, nargs="+", help="Filepath to metatile_coord_dict files to use")
+    parser.add_argument('--player_img', type=str, help="Player image", default='block')
+    args = parser.parse_args()
+    main(args.save_filename, args.metatile_id_map_file, args.metatile_coords_dict_files, args.player_img)
+
+
+    # parser.add_argument('metatile_id_file', type=str, help='Filepath for metatile_id map')
+    # parser.add_argument('--games', type=str, nargs="+", help='List of games', default="")
+    # parser.add_argument('--levels', type=str, nargs="+", help='List of game levels', default="")
+    # parser.add_argument('--player_img', type=str, help="Player image", default='block')
+    # parser.add_argument('--load_saved_files', const=True, nargs='?', type=bool, default=False)
+    # parser.add_argument('--outfile', type=str, help="Output filename", default=None)
+    # args = parser.parse_args()
+    #
+    # main(args.metatile_id_file, args.games, args.levels, args.player_img, args.load_saved_files, args.outfile)
