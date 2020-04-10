bcf9e62e107be83eee45204b1f3bc1af11350bfe
lee-vivian <lee-vivian@users.noreply.github.com>
2020-04-10 12:29:00 -0400

    Save work on updating gen_prolog.py

--- gen_prolog.py
+++ gen_prolog.py
@@ -1,56 +1,57 @@
 import networkx as nx
+import re
 import argparse
 
-from model.level import Level, TILE_DIM
-from model.metatile import Metatile, METATILE_TYPES
-import extract_constraints
+from model.level import TILE_DIM
+from model.metatile import METATILE_TYPES
 import utils
 
 # Acknowledgements and References:
 # - Legal adjacency rules based on NPCDev groundcollapse
-# - WFC in ASP implementation: based on Karth, I., & Smith, A. M. (2017). WaveFunctionCollapse is constraint solving in the wild. Proceedings of the 12th International Conference on the Foundations of Digital Games, 68. ACM.
+# - WFC in ASP implementation: based on Karth, I., & Smith, A. M. (2017). WaveFunctionCollapse is constraint solving
+#   in the wild. Proceedings of the 12th International Conference on the Foundations of Digital Games, 68. ACM.
 
 
-def main(game, level, player_img, level_width, level_height, debug, print_pl, min_perc_blocks, start_bottom_left):
+def parse_constraints_filepath(constraints_filename):
+    match = re.match(r'([^/]+)/metatile_constraints/([a-zA-Z0-9_-]+).pickle', constraints_filename)
+    level_saved_files_dir = match.group(1)
+    prolog_filename = match.group(2)
+    return level_saved_files_dir + "/", prolog_filename
 
-    print("Generating prolog file for level: %s ..." % level)
 
-    # Use training level dimensions if level_width or level_height are None
-    default_width, default_height = Level.get_level_dimensions_in_tiles(game, level)
-    if level_width is None:
-        level_width = default_width
-    if level_height is None:
-        level_height = default_height
+def main(tile_constraints_file, level_w, level_h, min_perc_blocks, start_bottom_left, debug, print_pl):
 
-    print("Default dimensions (%d, %d)" % (default_width, default_height))
-    print("Gen dimensions (%d, %d)" % (level_width, level_height))
+    print("Generating prolog file for constraints file: %s ..." % tile_constraints_file)
+    print("Generated level dimensions in tiles (%d, %d)" % (level_w, level_h))
 
-    # Setup save file directory
-    level_saved_files_dir = "level_saved_files_%s/" % player_img
-    outfile_name = "%s_%d_%d.pl" % (level, level_width, level_height)
-    outfile_path = utils.get_filepath(level_saved_files_dir + "level_prolog_files", outfile_name)
+    # Set up save file directory
+    level_saved_files_dir, prolog_filename = parse_constraints_filepath(tile_constraints_file)
+    prolog_filename = "%s_%d_%d" % (prolog_filename, level_w, level_h)
+    prolog_filepath = utils.get_filepath(level_saved_files_dir + "prolog_files", "%s.pl" % prolog_filename)
 
-    # Get training level unique metatiles
-    metatiles = Metatile.get_unique_metatiles_for_level(game, level, player_img)
-
-    # Get training level tileset constraints dictionary
-    metatile_id_map_file = utils.get_filepath(level_saved_files_dir + "metatile_id_maps", "%s.pickle" % level)
-    tileset_dict = extract_constraints.get_tileset_dict(metatile_id_map_file, game, level, player_img)
-    tile_constraints_dict = tileset_dict.get("tiles")
+    # Load in tile constraints dictionary
+    tile_id_constraints_dict = utils.read_pickle(tile_constraints_file)
+    # tile_id_constraints_dict[tile_id] = {
+    #     "type": metatile.type,
+    #     "graph": metatile.graph_as_dict,
+    #     "adjacent": {
+    #         TOP: [], BOTTOM: [], LEFT: [], RIGHT: [], TOP_LEFT: [], BOTTOM_LEFT: [], TOP_RIGHT: [], BOTTOM_RIGHT: []
+    #     }
+    # }
 
     # Generate prolog statements
     prolog_statements = ""
-    prolog_statements += "dim_width(0..%d).\n" % (level_width - 1)
-    prolog_statements += "dim_height(0..%d).\n" % (level_height - 1)
-    prolog_statements += "dim_metatiles(1..%d).\n" % (len(metatiles))
+    prolog_statements += "dim_width(0..%d).\n" % (level_w - 1)
+    prolog_statements += "dim_height(0..%d).\n" % (level_h - 1)
+    prolog_statements += "dim_metatiles(1..%d).\n" % (len(tile_id_constraints_dict.keys()))
 
     # Create tile facts
     create_tiles_statement = "tile(TX,TY) :- dim_width(TX), dim_height(TY)."
     prolog_statements += create_tiles_statement + "\n"
 
     # Limit number of metatile assignments per tile
-    limit = 0 if debug else 1
-    limit_metatile_per_tile_rule = "%d {assignment(TX, TY, MT) : metatile(MT) } 1 :- tile(TX,TY)." % limit
+    minimum = 0 if debug else 1
+    limit_metatile_per_tile_rule = "%d {assignment(TX, TY, MT) : metatile(MT) } 1 :- tile(TX,TY)." % minimum
     prolog_statements += limit_metatile_per_tile_rule + "\n"
 
     # Create tile adjacency rules
@@ -66,67 +67,67 @@ def main(game, level, player_img, level_width, level_height, debug, print_pl, mi
     symmetric_legal_adjacencies_rule = "legal(DX, DY, P1, P2) :- legal(IDX, IDY, P2, P1), IDX == -DX, IDY == -DY."
     prolog_statements += symmetric_legal_adjacencies_rule + "\n"
 
-    # Dictionary: {metatile_type: list of metatile ids}
-    metatile_type_ids = {}
+    metatile_type_ids_map = {}
     for type in METATILE_TYPES:
-        metatile_type_ids[type] = []
-
+        metatile_type_ids_map[type] = []  # {metatile_type: list-metatile-ids}
     start_state = None
     goal_state = None
 
-    # Create metatile facts, metatile_type facts, and legal neighbor statements for the specified tileset
-    for metatile_id, metatile_info in tile_constraints_dict.items():  # for each unique metatile
+    # Create metatile facts, metatile_type facts, and legal neighbor statements for the specified tile constraints dict
 
-        metatile_fact = "metatile(%s)." % metatile_id
+    for tile_id, tile_constraints in tile_id_constraints_dict.items():
+
+        metatile_fact = "metatile(%s)." % tile_id
         prolog_statements += metatile_fact + "\n"
 
-        metatile_type = metatile_info.get("type")
-        metatile_type_ids[metatile_type].append(metatile_id)
+        metatile_type = tile_constraints.get('type')
+        metatile_type_ids_map[metatile_type].append(tile_id)
 
-        metatile_graph_as_dict = eval(metatile_info.get("graph"))
-        metatile_graph = nx.DiGraph(metatile_graph_as_dict)
+        metatile_graph = nx.DiGraph(tile_constraints.get('graph'))
 
         # Create state and link rules based on metatile graph
+
         for edge in metatile_graph.edges():
             src_state = eval(edge[0])
             dest_state = eval(edge[1])
             src_x, src_y, dest_x, dest_y = src_state['x'], src_state['y'], dest_state['x'], dest_state['y']
 
-            state_rule = "state(%d+TX*%d, %d+TY*%d) :- assignment(TX,TY,%s)." % (src_x, TILE_DIM, src_y, TILE_DIM, metatile_id)
+            state_rule = "state(%d+TX*%d, %d+TY*%d) :- assignment(TX,TY,%s)." % (src_x, TILE_DIM, src_y, TILE_DIM, tile_id)
             link_rule = "link(%d+TX*%d, %d+TY*%d, %d+TX*%d, %d+TY*%d) :- assignment(TX,TY,%s)." % \
-                        (src_x, TILE_DIM, src_y, TILE_DIM, dest_x, TILE_DIM, dest_y, TILE_DIM, metatile_id)
+                        (src_x, TILE_DIM, src_y, TILE_DIM, dest_x, TILE_DIM, dest_y, TILE_DIM, tile_id)
 
             prolog_statements += state_rule + "\n"
             prolog_statements += link_rule + "\n"
 
-            # define start state in prolog
+            # Define start fact in prolog
             if start_state is None and src_state['is_start']:
-                start_rule = "start(%d+TX*%d, %d+TY*%d) :- assignment(TX,TY,%s)." % (src_x, TILE_DIM, src_y, TILE_DIM, metatile_id)
+                start_rule = "start(%d+TX*%d, %d+TY*%d) :- assignment(TX,TY,%s)." % (src_x, TILE_DIM, src_y, TILE_DIM, tile_id)
                 prolog_statements += start_rule + "\n"
                 start_state = src_state
 
-            # define goal state in prolog
+            # Define goal fact in prolog
             if goal_state is None and src_state['goal_reached']:
-                goal_rule = "goal(%d+TX*%d, %d+TY*%d) :- assignment(TX,TY,%s)." % (src_x, TILE_DIM, src_y, TILE_DIM, metatile_id)
+                goal_rule = "goal(%d+TX*%d, %d+TY*%d) :- assignment(TX,TY,%s)." % (src_x, TILE_DIM, src_y, TILE_DIM, tile_id)
                 prolog_statements += goal_rule + "\n"
                 goal_state = src_state
 
         # Create legal rules based on valid adjacent tiles
-        for direction, adjacent_tiles in metatile_info.get("adjacent").items():  # for each adjacent dir
-            DX, DY = eval(direction)
+        for direction, adjacent_tiles in tile_constraints.get("adjacent").items():  # for each adjacent dir
+            dx, dy = eval(direction)
 
             for adjacent_id in adjacent_tiles:  # for each adjacent metatile
                 legal_statement = "legal(DX, DY, MT1, MT2) :- DX == %d, DY == %d, metatile(MT1), metatile(MT2), " \
-                                  "MT1 == %s, MT2 == %s." % (DX, DY, metatile_id, adjacent_id)
+                                  "MT1 == %s, MT2 == %s." % (dx, dy, tile_id, adjacent_id)
                 prolog_statements += legal_statement + "\n"
 
     # Link can only exist if the src and dest states exist, otherwise solution is not valid
     link_exists_rule = ":- link(X1,Y1,X2,Y2), state(X1,Y1), not state(X2,Y2)."
     prolog_statements += link_exists_rule
 
-    block_tile_id = metatile_type_ids.get("block")[0]
-    start_tile_id = metatile_type_ids.get("start")[0]
-    goal_tile_id = metatile_type_ids.get("goal")[0]
+    # Get block, start, and goal tile_ids
+    block_tile_id = metatile_type_ids_map.get("block")[0]
+    start_tile_id = metatile_type_ids_map.get("start")[0]
+    goal_tile_id = metatile_type_ids_map.get("goal")[0]
 
     # Start state is inherently reachable
     start_reachable_rule = "reachable(X,Y) :- start(X,Y)."
@@ -141,11 +142,12 @@ def main(game, level, player_img, level_width, level_height, debug, print_pl, mi
     prolog_statements += state_reachable_rule + "\n"
 
     # Set border tiles to be block tiles
+    # TODO update here and below
     block_tile_coords = []
-    for x in range(level_width):
-        block_tile_coords += [(x, 0), (x, level_height-1)]
-    for y in range(level_height):
-        block_tile_coords += [(0, y), (level_width-1, y)]
+    for x in range(level_w):
+        block_tile_coords += [(x, 0), (x, level_h-1)]
+    for y in range(level_h):
+        block_tile_coords += [(0, y), (level_x-1, y)]
     for x, y in list(set(block_tile_coords)):
         block_tile_assignment = "assignment(%d, %d, %s)." % (x, y, block_tile_id)
         prolog_statements += block_tile_assignment + "\n"
@@ -198,17 +200,16 @@ def main(game, level, player_img, level_width, level_height, debug, print_pl, mi
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Enumerate state graph for the given level')
-    parser.add_argument('game', type=str, help='Name of the game')
-    parser.add_argument('level', type=str, help='Name of the level')
-    parser.add_argument('--player_img', type=str, help='Player image', default='block')
-    parser.add_argument('--level_width', type=int, help='Number of tiles in a row', default=None)
-    parser.add_argument('--level_height', type=int, help='Number of tiles in a column', default=None)
-    parser.add_argument('--debug', const=True, nargs='?', type=bool, help='Allow blank tiles if no suitable assignment can be found', default=False)
-    parser.add_argument('--print_pl', const=True, nargs='?', type=bool, help='Print prolog statements to console', default=False)
-    parser.add_argument('--min_perc_blocks', type=int, help='Percent number of block tiles in a level', default=None)
-    parser.add_argument('--start_bottom_left', const=True, nargs='?', type=bool, default=False)
+    parser = argparse.ArgumentParser(description='Generate prolog file')
+    parser.add_argument('tile_constraints_file', type=str, help="File path of the tile constraints dictionary to use")
+    parser.add_argument('level_w', type=int, help="Number of tiles in a row")
+    parser.add_argument('level_h', type=int, help="Number of tiles in a column")
+    parser.add_argument('--min_perc_blocks', type=int, default=None, help='Minimum percentage of block tiles in a level')
+    parser.add_argument('--start_bottom_left', const=True, nargs='?', type=bool, default=False, help='Fix start position to the bottom left of the level')
+    parser.add_argument('--debug', const=True, nargs='?', type=bool, default=False, help='Allow empty tiles if no suitable assignment can be found')
+    parser.add_argument('--print_pl', const=True, nargs='?', type=bool, default=False, help='Print prolog statements to console')
+
     args = parser.parse_args()
 
-    main(args.game, args.level, args.player_img, args.level_width, args.level_height, args.debug, args.print_pl,
-         args.min_perc_blocks, args.start_bottom_left)
+    main(args.tile_constraints_file, args.level_w, args.level_h, args.min_perc_blocks, args.start_bottom_left,
+         args.debug, args.print_pl)
