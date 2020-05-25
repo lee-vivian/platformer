c0f11a98e079af625b2eacba2d950f26605ae2bc
lee-vivian <lee-vivian@users.noreply.github.com>
2020-05-25 03:11:12 -0400

    Add script to auto-run process and solver scripts on specified levels

---
+++ trial.py
@@ -0,0 +1,20 @@
+import os
+
+levels = ["mario-1-1", "mario-1-2", "mario-1-3"]
+
+# ----- PROCESS -----
+for level in levels:
+    os.system("pypy3 main.py platformer super_mario_bros %s --process" % level)
+
+# ----- SOLVE -----
+solver_widths = [50, 100, 150]
+max_sol = 2
+threads = 12
+
+for level in levels:
+    prolog_file = "level_saved_files_block/prolog_files/%s.pl" % level
+    for w in solver_widths:
+        config_file = "config-%s-%d.json" % (level, w)
+        os.system("python run_solver.py %s %s --max_sol %d --threads %d --save --validate > %s-%d.txt" % (
+            prolog_file, config_file, max_sol, threads, level, w))
+        print("Saved to: %s-%d.txt" % (level, w))
