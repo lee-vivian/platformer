df9525329edc561a286abc3adabcc6907141b618
lee-vivian <lee-vivian@users.noreply.github.com>
2020-05-26 18:37:24 -0400

    Clean up trial.py

--- trial.py
+++ trial.py
@@ -4,18 +4,21 @@ import argparse
 from model.level import Level
 import utils
 
+# SOLVER SPECIFICATIONS
 GAME = "super_mario_bros"
 LEVELS = ["mario-1-1", "mario-1-2", "mario-1-3"]
-WIDTHS = [50, 100, 150]
-CONFIG_SUFFIXES = []  # prefix = config-level, suffix = config specifications
 MAX_SOL = 2
 THREADS = 12
 
-# # ----- CREATE CONFIG FILE -----
+# CONFIG SPECIFICATIONS
+WIDTHS = [50, 100, 150]
+PERC_TILE_COMBOS = [['block'], ['bonus'], ['hazard'], ['block', 'bonus', 'hazard']]
+PERC_TILE_DEVIATION = 5
+
 
 def create_config_file(config_suffix, game, level, perc_width=100, perc_height=100, start_within=10, goal_within=10,
                        reachable_platforms=True, reachable_bonus_tiles=True,
-                       perc_tile_deviation=5, perc_tile_types=None):
+                       perc_tile_deviation=PERC_TILE_DEVIATION, perc_tile_types=None):
 
     training_width, training_height = Level.get_level_dimensions_in_tiles(game, level)
     config_width = int(training_width * perc_width/100)
@@ -54,9 +57,19 @@ def create_config_file(config_suffix, game, level, perc_width=100, perc_height=1
     utils.write_json(config_file, {"config": config})
 
 
-
-
 def main(process, solve):
+
+    # CREATE CONFIG FILES
+    config_suffixes = []
+    for level in LEVELS:
+        for perc_tile_combo in PERC_TILE_COMBOS:
+            for width in WIDTHS:
+                suffix_contents = [str(width)] + perc_tile_combo
+                config_suffix = "_".join(suffix_contents)
+                create_config_file(config_suffix=config_suffix, game=GAME, level=level, perc_width=width,
+                                   perc_tile_types=perc_tile_combo)
+                config_suffixes.append(config_suffix)
+    config_suffixes = list(set(config_suffixes))
 
     if process:
         print("----- PROCESSING -----")
@@ -64,7 +77,7 @@ def main(process, solve):
 
         for level in LEVELS:
             process_file = utils.get_filepath(process_dir, "%s.txt" % level)
-            os.system("(time pypy3 main.py platformer super_mario_bros %s --process) > %s 2>&1" % (level, process_file))
+            os.system("(time pypy3 main.py platformer %s %s --process) > %s 2>&1" % (GAME, level, process_file))
             print("Saved to: %s" % process_file)
 
     if solve:
@@ -75,8 +88,8 @@ def main(process, solve):
             prolog_file = "level_saved_files_block/prolog_files/%s.pl" % level
             solve_level_dir = utils.get_directory("%s/%s" % (solve_dir, level))
 
-            for config_suffix in CONFIG_SUFFIXES:
-                config_file = "solver_config/%s-%s.json" % (level, config_suffix)
+            for config_suffix in config_suffixes:
+                config_file = "solver_config/config-%s-%s.json" % (level, config_suffix)
                 solve_file = utils.get_filepath(solve_level_dir, "%s.txt" % config_suffix)
 
                 os.system("(time python run_solver.py %s %s --max_sol %d --threads %d --save --validate) > %s 2>&1" % (
@@ -86,15 +99,9 @@ def main(process, solve):
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description='Make system calls to process and run solver for specified levels')
     parser.add_argument('--process', const=True, nargs='?', type=bool, default=False)
     parser.add_argument('--solve', const=True, nargs='?', type=bool, default=False)
     args = parser.parse_args()
     main(process=args.process, solve=args.solve)
 
-    # CREATE CONFIG FILES
-    for width in WIDTHS:
-        create_config_file(config_suffix="%d_blocks" % width, game="super_mario_bros", level="mario-1-1", perc_width=width,
-                           perc_tile_types=['block'])
-    exit(0)
